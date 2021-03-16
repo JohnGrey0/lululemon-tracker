635af71d5fd019cce8bbbb9ee9a3b28203585381
John Grey <github@johnh.org>
2021-03-16 11:46:49 -0400

    added better output and decreased delay

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from os import system, name, getenv
 from twilio.rest import Client
 import time
+from datetime import datetime
 
 
 def send_text(message):
@@ -17,27 +18,30 @@ def send_text(message):
 def get_page_html(url):
     headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
     page = requests.get(url, headers=headers)
-    print(page.status_code)
-    return page.content
+    if page.status_code == 200:
+        return page.content
+    return None
 
 
 def check_item_in_stock(page_html):
     soup = BeautifulSoup(page_html, 'html.parser')
-    out_of_stock_divs = soup.findAll("button", {"data-lulu-track": "pdp-add-to-bag-regular-enabled"})  # <--- change "text" to div
-    print(out_of_stock_divs)
+    out_of_stock_divs = soup.findAll("button", {"data-lulu-track": "pdp-add-to-bag-regular-enabled"})
+    print("{}".format(out_of_stock_divs))
     return len(out_of_stock_divs) != 0
 
 def check_inventory(url):
     page_html = get_page_html(url)
-    if check_item_in_stock(page_html):
-        print("In stock")
-        send_text("Lululemon Align Cassis IN STOCK {url}".format(url=url))
-        exit()
-    else:
-        print("Out of stock")
+    if page_html is not None:
+        if check_item_in_stock(page_html):
+            print("{:88} - [In stock]".format(url))
+            send_text("Lululemon Align Cassis IN STOCK {url}".format(url=url))
+            exit()
+        else:
+            print("{:88} - [Out of stock]".format(url))
 
 if __name__ == "__main__":
     url = "https://shop.lululemon.com/p/women-pants/Align-Pant-Tall/_/prod9410067?color=26950&sz=12"
     while True:
+        print("{} - Checking inventory...".format(datetime.now()))
         check_inventory(url)
-        time.sleep(1800)
+        time.sleep(600)
