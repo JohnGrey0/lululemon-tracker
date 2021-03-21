44111f281befed58d2c2cd160471bd94b1ebd5bd
John Grey <github@johnh.org>
2021-03-21 14:44:12 -0400

    added retry

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def get_page_html(url):
     headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
     page = requests.get(url, headers=headers)
     if page.status_code == 200:
-        return page.content
-    return None
+        return page.content, page.status_code
+    return None, page.status_code
 
 
 def check_item_in_stock(page_html):
@@ -29,8 +29,21 @@ def check_item_in_stock(page_html):
     print("{}".format(out_of_stock_divs))
     return len(out_of_stock_divs) != 0
 
+
 def check_inventory(url):
-    page_html = get_page_html(url)
+    status_code = 0
+    page_html = None
+    counter = 0
+    max_retries = 24
+    sleep = 300
+    while status_code != 200 and page_html is None and counter < max_retries:
+        counter += 1
+        try:
+            print("{} - Checking products for availability attempt #{}...".format(str(datetime.now()), counter))
+            page_html, status_code = get_page_html(url)
+        except Exception as e:
+            time.sleep(sleep)
+
     if page_html is not None:
         if check_item_in_stock(page_html):
             print("{:88} - [In stock]".format(url))
@@ -42,6 +55,5 @@ def check_inventory(url):
 if __name__ == "__main__":
     url = "https://shop.lululemon.com/p/women-pants/Align-Pant-Tall/_/prod9410067?color=26950&sz=12"
     while True:
-        print("{} - Checking inventory...".format(datetime.now()))
         check_inventory(url)
         time.sleep(600)
